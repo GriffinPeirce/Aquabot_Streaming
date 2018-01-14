d68fef054d7ef9c1d66334ba868f59a49511fb14
GriffinPeirce <griff.peirce@gmail.com>
2018-01-14 08:34:23 +0000

    Resolved UDPsrc failure. Added encoder payload elements

--- GstTest.cpp
+++ GstTest.cpp
@@ -3,46 +3,31 @@
 #include <gst/gst.h>
 #include <glib.h>
 
+void confirmVersion();
+
 int main(int argc, char *argv[]){
 
 GMainLoop *loop;
 GstElementFactory *factory;
-GstElement *pipeline,*source,*filter,*sink,*element;
-gchar *name;
+GstElement *pipeline,*source,*filter,*encoder,*payloader,*sink;
 
-const gchar *nano_str;
-guint major, minor, micro, nano;
 
 gst_init (&argc, &argv);
 loop = g_main_loop_new(NULL,FALSE);
 
-gst_version(&major,&minor,&micro,&nano);
-
-if(nano == 1)
-   nano_str = "(CVS)";
-else if (nano == 2)
-   nano_str = "(Prerelease)";
-else
-   nano_str = "";
-
-printf("Built with GStreamer %d.%d.%d %s\n", major, minor, micro, nano_str);
+confirmVersion();
 
 pipeline = gst_pipeline_new("Aquabot_Cam_1");
-source  = gst_element_factory_make("v4l2src","source");
-filter = gst_element_factory_make("identity","filter");
-//sink = gst_element_factory_make("updsink","sink");
+source  = gst_element_factory_make("v4l2src","c270-source");
+filter = gst_element_factory_make("identity","capture-filter");
+encoder = gst_element_factory_make("jpegenc","jpeg-encoder");
+payloader = gst_element_factory_make("rtpjpegpay","jpeg-payloader");
+sink = gst_element_factory_make("udpsink","network-sink");
 
- factory = gst_element_factory_find ("udpsink");
-  if (!factory) {
-    g_print ("Failed to find factory of type 'udpsink'\n");
-    return -1;
-  }
-  sink = gst_element_factory_create (factory, "udpsink");
-  if (!sink) {
-    g_print ("Failed to create element, even though its factory exists!\n");
-    return -1;
-  }
-
+if(!pipeline || !source || !filter || !encoder || !payloader || !sink){
+	g_printerr("Failed to create one element. Exiting.\n");
+	return -1;
+}
 g_object_set(G_OBJECT(source),"device","/dev/video0",NULL);
 
 g_object_set(G_OBJECT(sink),"host","192.168.1.255",NULL);
@@ -70,4 +55,21 @@ g_main_loop_unref(loop);
 //gst_object_unref(GST_OBJECT(sink));
 
 return 0;
+}
+
+void confirmVersion(){
+
+const gchar *nano_str;
+guint major, minor, micro, nano;
+
+gst_version(&major,&minor,&micro,&nano);
+
+if(nano == 1)
+   nano_str = "(CVS)";
+else if (nano == 2)
+   nano_str = "(Prerelease)";
+else
+   nano_str = "";
+
+printf("Built with GStreamer %d.%d.%d %s\n", major, minor, micro, nano_str);
 }
