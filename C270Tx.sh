659310f550092abfd97561910da02501634e8794
GriffinPeirce <griff.peirce@gmail.com>
2018-02-20 01:39:40 -0800

    Removed outdated scripts

--- C270Tx.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gst-launch-1.0 -v v4l2src device="/dev/video1" ! video/x-raw,width=1280,height=720 ! tee name="s" ! queue !  jpegenc ! rtpjpegpay ! udpsink host="192.168.1.255" port="7331" s. ! queue ! xvimagesink
