eece18615b0e10bd107f5805b55eaae2fa26bed5
GriffinPeirce <griff.peirce@gmail.com>
2018-02-20 02:30:09 -0800

    Working browser stream 10s latency w firefox; chrome https failiure

--- GstTest.sh
+++ GstTest.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-gst-launch-1.0 videotestsrc ! autovideosink
+gst-launch-1.0 -v\
+        v4l2src device="/dev/video0" \
+        ! tee name=alpha !queue !  videoconvert ! videoscale ! video/x-raw,width=320,height=240 \
+        ! clockoverlay shaded-background=true font-desc="Sans 38" \
+        ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=7331 alpha. ! queue !  clockoverlay shaded-background=true font-desc="Sans 38" ! autovideosink
